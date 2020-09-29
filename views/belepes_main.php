f1ee40adde6095e8ded8bd10ea1ecdeee27c28cf
Dániel Benkő <daniel.benko911@gmail.com>
2020-09-29 10:25:51 +0200

    0.0.0.3 - Project migrated to Laravel.

--- views/belepes_main.php
+++
@@ -1,8 +0,0 @@
-<h2>Belépés</h2>
-
-<form action="<?= SITE_ROOT ?>beleptet" method="post">
-    <label for="login">Felhasználó:</label><input type="text" name="login" id="login"><br>
-    <label for="password">Jelszó:</label><input type="password" name="password" id="password"><br>
-    <input type="submit" value="Küldés">
-</form>
-<h2><br><?= (isset($viewData['uzenet']) ? $viewData['uzenet'] : "") ?><br></h2>
