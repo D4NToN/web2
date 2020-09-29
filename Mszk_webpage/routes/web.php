316747b2bec6e0661e5efb2a34f5b66f1573b005
Dániel Benkő <daniel.benko911@gmail.com>
2020-09-29 15:11:04 +0200

    0.0.0.5 - Alpha main page

--- Mszk_webpage/routes/web.php
+++ Mszk_webpage/routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('welcome');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/new_post', function () {
+    return view('new_post');
+});
+
+Route::get('/services', function () {
+    return view('services');
+});
+
+Route::get('/register', function () {
+    return view('register');
 });
