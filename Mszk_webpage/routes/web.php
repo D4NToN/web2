aedf523c1fd9f9bf2e39635b0d6c12a2dca153c3
Dániel Benkő <daniel.benko911@gmail.com>
2020-09-30 09:35:58 +0200

    0.0.0.6 - Login page done.

--- Mszk_webpage/routes/web.php
+++ Mszk_webpage/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/home', function () {
     return view('welcome');
 });
@@ -32,3 +36,7 @@ Route::get('/services', function () {
 Route::get('/register', function () {
     return view('register');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
